83979ce300bb39f7c58e74c36100e6c51ce56790
professor Etec <professor@etec.com.br>
2026-09-14 22:51:03 -0300

    Alterei as configs do projeto

--- app/src/main/java/com/etecbauru/myapplication/MainActivity.kt
+++ app/src/main/java/com/etecbauru/myapplication/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+
+        setContentView(R.layout.tela_pesquisa_satisfacao)
+
+
 
     }
 }
