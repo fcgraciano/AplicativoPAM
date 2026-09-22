9ab77db4336b90666f809375fa9126c3991d5a3e
pcetec <pc@etec.com>
2026-09-21 22:47:50 -0300

    Aula do IMC

--- app/src/main/java/com/etecbauru/myapplication/MainActivity.kt
+++ app/src/main/java/com/etecbauru/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.etecbauru.myapplication
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,13 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.aulaid)
 
-        val botao = findViewById<Button>(R.id.btnProxima)
-        val texto = findViewById<EditText>(R.id.editValor)
-
-        botao.text = "Novo Texto"
+        val notapP1 = findViewById<EditText>(R.id.edtNota1)
+        val notapP2 = findViewById<EditText>(R.id.edtNota2)
+        val resultado = findViewById<TextView>(R.id.txtResultado)
+        val botao = findViewById<Button>(R.id.btnCalcular)
 
         botao.setOnClickListener {
-           botao.text = texto.text
+            val soma = ( notapP1.text.toString().toFloat()
+                        + notapP2.text.toString().toFloat()
+                       )
+
+            val media = soma / 2
+
+           resultado.text = "Média é" + media
+
         }
     }
 }
